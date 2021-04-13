1b2d111f971afae45efdcc6d3de8ebf902216ac5
Freddy <you@example.com>
2021-04-13 16:50:21 -0400

    confirmo

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,47 @@ function mensaje() {
     alert('Yo Soy :' + nombre);
 }
 
+function validarForm() {
+    var resp = validarut();
+    if (resp==false) {
+        return false;
+    }
+    resp = validaFecha();
+    if (resp==false) {
+        return false;
+    }
+}
+
+////////////////////////////// funciones de validacion
+function validaFecha() {
+    var fechaUsuario= document.getElementById('txtFechaNaci').value;
+    console.log('Fecha Usuario:'+fechaUsuario);
+    var fechaSistema = new Date();
+    console.log('Fecha Sistema:'+fechaSistema);
+    ///////////////////////////////////////////////
+    var ano = fechaUsuario.slice(0,4);
+    var mes = fechaUsuario.slice(5,7);
+    var dia = fechaUsuario.slice(8,10);
+    console.log('Año:'+ano+' Mes:'+mes+' Dia:'+dia);
+    ///////////////////////////////////////////////
+    var fechaNuevaUsuario = new Date(ano,(mes-1),dia);
+    console.log('Nueva Fecha Usuario:'+fechaNuevaUsuario);
+    if (fechaNuevaUsuario >= fechaSistema) {
+        alert('Fecha incorrecta');
+        return false;
+    }
+    ///////////////////////////////////////////////////
+    var unDia = 24 * 60 * 60 * 1000;//cuantos milisegundos es un dia
+    var diferencia_dias =Math.trunc( ( fechaSistema.getTime() - fechaNuevaUsuario.getTime() ) / unDia);
+    console.log('Dias:'+diferencia_dias);
+    var anos =Math.trunc( diferencia_dias / 365 );
+    console.log('Años:'+anos);
+    if (anos<18) {
+        alert('es menor de edad, usted tiene '+anos+ ' años de edad');
+        return false;
+    }
+    return true;
+}
 function validarut() {
     var rut = document.getElementById('txtRut').value;
     if ( rut.length !=10 ) {
@@ -14,6 +55,7 @@ function validarut() {
     for (let index = 0; index < 8; index++) {
         var carac= rut.slice(index,index+1);
         //alert(carac + ' x '+ num);
+        console.log(carac + ' x '+ num);
         suma = suma + ( carac * num );
         num=num-1;
         if (num == 1) {
@@ -21,6 +63,7 @@ function validarut() {
         }
     }
     // alert(' Total: '+suma);
+    console.log(' Total: '+suma);
     var resto = suma % 11;
     var dv = 11 - resto;
     
@@ -31,6 +74,11 @@ function validarut() {
             dv=0;
         }
     }
-    alert('DV:'+dv);
-    
+    console.log('DV:'+dv);
+    var dv_usuario = rut.slice(-1).toUpperCase();
+    if (dv != dv_usuario) {
+        alert('Rut incorrecto');
+        return false;
+    }
+    return true;
 }
